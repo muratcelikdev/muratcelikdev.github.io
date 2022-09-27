9f8d3a1b3571429bc243c50725beaf7238d336c3
Murat Çelik <muratcelikdev@gmail.com>
2022-09-27 09:46:17 +0300

    add coming soon contetn

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,40 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Typography from 'src/components/typography';
+import styled from 'styled-components';
+
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  background-color: ${({ theme }) => theme?.colors.lightGrey.primary};
+`;
+
+const Heading = styled(Typography).attrs({ type: 'heading' })`
+  background: ${({ theme }) =>
+    `linear-gradient(${theme?.colors.lightGrey.primary}, ${theme?.colors.black.default});`};
+  background-clip: text;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+`;
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Murat Çelik - Frontend Developer</title>
         <meta name="description" content="Murat Çelik - Frontend Developer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Coming <span>Soon!</span>
-        </h1>
-      </main>
-    </div>
+      <MainContainer>
+        <Heading>Coming Soon</Heading>
+      </MainContainer>
+    </>
   );
 };
 
