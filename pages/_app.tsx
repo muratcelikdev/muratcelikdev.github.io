381a3cd9785ae632ceaeba4119ef0b63b50e70f4
Murat Çelik <muratcelikdev@gmail.com>
2022-09-27 08:36:44 +0300

    add fonts to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 
+import GlobalStyle from 'src/theme/GlobalStyle';
 import ThemeProvider from 'src/theme/ThemeProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
   );
---
+++ src/theme/fonts.ts
@@ -0,0 +1,52 @@
+import { css } from 'styled-components';
+
+export const fontFace = css`
+  /* ExtraLight */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-ExtraLight.ttf');
+    font-weight: 200;
+  }
+
+  /* Light */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-Light.ttf');
+    font-weight: 300;
+  }
+
+  /* Regular */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-Regular.ttf');
+    font-weight: 200;
+  }
+
+  /* Medium */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-Medium.ttf');
+    font-weight: 500;
+  }
+
+  /* SemiBold */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-SemiBold.ttf');
+    font-weight: 600;
+  }
+
+  /* Bold */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-Bold.ttf');
+    font-weight: 700;
+  }
+
+  /* ExtraBold */
+  @font-face {
+    font-family: 'PlusJakarta';
+    src: url('/assets/fonts/PlusJakartaSans-ExtraBold.ttf');
+    font-weight: 800;
+  }
+`;
